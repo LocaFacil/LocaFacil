f039d762b1f541d0a15a2ed801b90fe45a3a0d60
LocaFacil <locafacilsuporte@gmai.com>
2021-09-09 19:56:41 -0300

    Parte de recuperação de senha tirando os status2

--- src/main/java/TechNinjas/LocaFacil/app/resources/PasswordResource.java
+++ src/main/java/TechNinjas/LocaFacil/app/resources/PasswordResource.java
@@ -87,10 +87,10 @@ public class PasswordResource {
         String subject = "Aqui está o link para resetar a senha";
         String content = "<p>Olá,</p>"
                 + "<p>Você esqueceu sua senha?</p>"
-                + "<p>Aqui está uma senha temporaria para pode usar:</p>"
-                + "<p>Senha: " + pass + "</p>"
+                + "<p>Aqui está uma senha temporaria para poder usar:</p>"
+                + "<p>Senha: <h2><font color=\"#fc9722\">" + pass + "</font></h2></p>"
                 + "<br>"
-                + "<p>Após entrar no sistema, você deve mudar sua senha para uma desejada"
+                + "<p>Após entrar no sistema, você deve mudar sua senha para uma senha desejada, "
                 + "vá para (Alterar Senha).</p>";
 
         helper.setSubject(subject);
